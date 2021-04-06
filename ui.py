e48f0f804ec84b93163acfba076f744de0c932d0
celineaubuchon <celine_aubuchon@brown.edu>
2021-04-06 18:16:31 -0400

    user interaction with keypresses and a first draft of a flying saucer

--- ui.py
+++ ui.py
@@ -5,9 +5,30 @@ from OpenGL.GLU import *
 from shapes import * # script I made that contains drawing functions for shapes
 from AsteroidField import AsteroidField # class representing an asteroid field
 from utilities import *
-
+def handle_keypress(event):
+    valid = True
+    speed = 0.1
+    key = event.key
+    if event.type == pygame.KEYDOWN:
+        # handle key down
+        if key == K_a:
+            x = speed; y = 0.0
+        elif key == K_d:
+            x = -speed; y = 0.0
+        elif key == K_w:
+            x = 0.0; y = -speed
+        elif key == K_s:
+            x = 0.0; y = speed
+        else:
+            valid = False; x = 0.0; y = 0.0
+    if event.type == pygame.KEYUP:
+        x = 0.0; y = 0.0
+    return (x, y, valid)
+            
+        
+    
+    return (x, y)
 def main():
-
     pygame.init() # initializes pygame
     display = (800, 600) # width and height of display box
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL) # sets up the display window
@@ -20,27 +41,33 @@ def main():
     glTranslatef(0.0, 0.0, camera_displacement) # zoom out 
     glRotatef(0.0, 0.0, 0.0, 0.0) # not doing anything right now
 
+    #initialize ship object
+    [ship_verts, ship_edges] = load_mesh('objs/ship.obj')
     #initialize asteroid field 
     asteroid_field = AsteroidField(10) # takes in number of asteroids
 
+    # translates scene (moves the ship through space)
+    x = 0.0
+    y = 0.0
+    z = 0.2
     #infinite loop
     while True:
+    
         ############################ MANAGE EVENTS ##############################
         for event in pygame.event.get():
             if event.type == pygame.QUIT: # if user closes the window
                 pygame.quit()
                 quit()
+            if (event.type == pygame.KEYDOWN) or (event.type == pygame.KEYUP):
+                [new_x, new_y, valid] = handle_keypress(event)
+                if valid:
+                    x = new_x; y = new_y
         #########################################################################
         ############################ DRAW GL SCENE ##############################
         
         # clear color and depth buffers at the beginning of current frame
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
-        # translates scene (moves the ship through space)
-        z = 0.1
-        #glTranslatef(0.0, 0.0, z) # speed is set by z, x and y could be used for steering?
-        
-        gone = 0
         # draws each asteroid
         for asteroid in asteroid_field.asteroids:
             (r_x, r_y, r_z) = asteroid.rotation_data
@@ -50,21 +77,30 @@ def main():
             # permanently rotates each vertex about the origin
             asteroid.rotate((r_x, r_y, r_z))
 
-            # updates the location of the asteroid center
-            c = asteroid.center
-            asteroid.center = (c[0], c[1], c[2] + z)
-            asteroid.translation_data = (t_x, t_y, t_z + z)
             # ugh, I just had a sign error but now its working
             if(asteroid.center[2] + camera_displacement > 5):
                 asteroid_field.del_asteroid(asteroid)
                 asteroid_field.add_asteroid()
+
             # draws the current asteroid
             glPushMatrix()
             glScalef(s_x, s_y, s_z)
             glTranslatef(t_x, t_y, t_z) # translates it to its location in space
             asteroid.draw() # draws it
             glPopMatrix()
-        #print(asteroid_field.asteroids[0].center)
+
+            # updates the location of the asteroid center
+            c = asteroid.center
+            asteroid.center = (c[0] + x, c[1] + y, c[2] + z)
+            asteroid.translation_data = (t_x + x, t_y + y, t_z + z)
+        
+        # draw the ship
+        glPushMatrix()
+        glScale(0.75, 0.75, 0.75)
+        glTranslate(0, -1.5, 0.5)
+        drawShapeLines(ship_verts, ship_edges, (0.75, 0.4, 0.6))
+        glPopMatrix()
+
         pygame.display.flip() # draw the buffers
         pygame.time.wait(20) 
         #########################################################################
